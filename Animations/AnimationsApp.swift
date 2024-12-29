213a9f2bd81500ef159d339b9b48e91080f035f9
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-29 22:31:54 +0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kevin Darmawan on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by Kevin Darmawan on 29/12/24.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
