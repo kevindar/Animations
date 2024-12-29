edd456659ff1e546eb3903b2e463e50db5523c3e
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-29 23:50:46 +0700

    Project 6 part 1 implicit/explicit/animate bindings day 32

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scaleAmount = 1.0
+    @State private var spinAmount: Double = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        print(scaleAmount)
+        
+        return VStack {
+            Stepper("Scale amount: \(scaleAmount.formatted())", value: $scaleAmount.animation(
+                .easeInOut(duration: 0.7)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap Me") {
+                withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                    spinAmount += 360
+                }
+                scaleAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(scaleAmount)
+            .overlay(Circle()
+                .stroke(.red)
+                .scaleEffect(scaleAmount)
+                .opacity(2 - scaleAmount)
+                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: scaleAmount)
+            )
+            .rotation3DEffect(.degrees(spinAmount), axis: (x:0, y:1, z:0))
+            .onAppear {
+                scaleAmount = 1
+            }
+            Spacer()
         }
-        .padding()
+        .padding(.all, 40)
     }
 }
 
