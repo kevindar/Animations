005de0590192457b7ade9307c489854ec873cb16
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-30 14:51:23 +0700

    Project 6 part 1 pokestop button ver

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,39 +11,43 @@ struct ContentView: View {
     
     @State private var scaleAmount = 1.0
     @State private var spinAmount: Double = 0
+    
     var body: some View {
-        print(scaleAmount)
-        
-        return VStack {
-            Stepper("Scale amount: \(scaleAmount.formatted())", value: $scaleAmount.animation(
-                .easeInOut(duration: 0.7)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-            Button("Tap Me") {
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    spinAmount += 360
+        ZStack {
+            VStack {
+                Stepper("Scale amount: \(scaleAmount.formatted())", value: $scaleAmount.animation(
+                    .easeInOut(duration: 0.7)
+                        .repeatCount(3, autoreverses: true)
+                ), in: 1...10, step: 1)
+                .zIndex(1) // Ensure Stepper stays on top
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                        spinAmount += 360
+                    }
+                    scaleAmount += 1
+                }) {
+                    Image("pokestop")
                 }
-                scaleAmount += 1
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(scaleAmount)
-            .overlay(Circle()
-                .stroke(.red)
+                .clipShape(Circle())
                 .scaleEffect(scaleAmount)
-                .opacity(2 - scaleAmount)
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: scaleAmount)
-            )
-            .rotation3DEffect(.degrees(spinAmount), axis: (x:0, y:1, z:0))
-            .onAppear {
-                scaleAmount = 1
+                .overlay(Circle()
+                    .stroke(.blue)
+                    .scaleEffect(scaleAmount)
+                    .opacity(2 - scaleAmount)
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: scaleAmount)
+                )
+                .rotation3DEffect(.degrees(spinAmount), axis: (x: 0, y: 1, z: 0))
+                .onAppear {
+                    scaleAmount = 1
+                }
+                
+                Spacer()
             }
-            Spacer()
+            .padding(.all, 40)
         }
-        .padding(.all, 40)
     }
 }
 
